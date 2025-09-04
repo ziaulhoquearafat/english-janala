bc5578833eb686e97239f47492111ce5b2874ee8
Arafat <mdarafat3167@gmail.com>
2025-09-04 17:28:03 +0600

    design header and lesson section with functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,24 @@
+const loadLessons = () => {
+  fetch('https://openapi.programming-hero.com/api/levels/all')
+    .then(res => res.json())
+    .then(json => displayLessons(json.data));
+};
 
+const displayLessons = lessons => {
+  const levelContainer = document.getElementById('level-container');
+  levelContainer.innerHTML = '';
+
+  for (let lesson of lessons) {
+    console.log(lesson);
+    const btnDiv = document.createElement('div');
+    btnDiv.innerHTML = `
+   
+        <button class="btn btn-outline btn-primary">
+           <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
+        </button>
+   `;
+    levelContainer.append(btnDiv);
+  }
+};
+
+loadLessons();
